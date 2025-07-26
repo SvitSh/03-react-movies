a4d762bc2d32e882875b02675341f7de9ae54a89
Svitlana Shevchenko <920314ss@gmail.com>
2025-07-26 14:33:40 +0300

    fix: refactor movieService to match task requirements

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -11,28 +11,14 @@ const axiosInstance = axios.create({
   },
 });
 
-export const fetchMovies = async (page = 1): Promise<MovieResponse> => {
-  const { data } = await axiosInstance.get<MovieResponse>("/movie/popular", {
-    params: {
-      language: "en-US",
-      include_adult: false,
-      page,
-    },
-  });
-  return data;
-};
-
-export const fetchSearchMovies = async (
-  query: string,
-  page = 1
-): Promise<MovieResponse> => {
+export const fetchMovies = async (query: string): Promise<MovieResponse> => {
   const { data } = await axiosInstance.get<MovieResponse>("/search/movie", {
     params: {
       language: "en-US",
       include_adult: false,
       query,
-      page,
     },
   });
+
   return data;
 };
